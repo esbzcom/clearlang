037ecd570b2f01953ba4eea405ccad4233c1cdc1
Felton <felton@esbz.com>
2025-09-14 10:56:50 -0400

    chore(codegen): remove unused import in lib.rs to silence warning

--- crates/codegen-wasm/src/lib.rs
+++ crates/codegen-wasm/src/lib.rs
@@ -1,9 +1,6 @@
-use anyhow::Result;
-
 pub mod intrinsics { pub mod strings; }
 pub mod ir;
 pub mod trivial;
 
 pub use ir::{emit_from_ir_with_opts, emit_from_ir, CodegenOpts};
 pub use trivial::emit_trivial_main;
-
